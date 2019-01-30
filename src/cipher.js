73186ca31d2c625ac5eebd75919f96b593b8eefb
Iana-Neri22 <iananeri@outlook.com>
2019-01-30 16:09:09 -0200

    alterações no projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,89 @@
-window.cipher = {
-  // ... 
-};
+function cipherEncode(message, offset) {
+
+  let codeCipher = '';
+
+  for (i = 0; i < message.length; i++) {
+    let codAsc = message.charCodeAt(i);
+
+    if (codAsc >= 65 && codAsc <= 90) {
+      let encodeMai = (codAsc - 65 + (offset % 26) + 26) % 26 + 65;
+
+      let codMai = String.fromCharCode(encodeMai);
+      codeCipher += codMai;
+    }
+
+    else if (codAsc >= 97 && codAsc <= 122) {
+      let encodeMin = (codAsc - 97 + (offset % 26) + 26) % 26 + 97;
+
+      let codMin = String.fromCharCode(encodeMin);
+      codeCipher += codMin;
+    }
+
+    else {
+      let others = String.fromCharCode(codAsc);
+      codeCipher += others;
+    }
+
+
+  }
+  return codeCipher;
+
+}
+
+function printEncode() {
+  let message = document.getElementById("message").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("FinalMessage").style.display = "block";
+
+  document.getElementById("resultCod").innerHTML = cipherEncode(message, offset);
+}
+
+
+function cipherDecode(message, offset) {
+
+  let decoderCipher = '';
+
+  for (i = 0; i < message.length; i++) {
+    let codAsc = message.charCodeAt(i);
+
+    if (codAsc >= 65 && codAsc <= 90) {
+      let decoderMai = (codAsc - 65 - (offset % 26) + 26) % 26 + 65;
+
+      let decMai = String.fromCharCode(decoderMai);
+      decoderCipher += decMai;
+    }
+
+    else if (codAsc >= 97 && codAsc <= 122) {
+      let decoderMin = (codAsc - 97 - (offset % 26) + 26) % 26 + 97;
+
+      let decMin = String.fromCharCode(decoderMin);
+      decoderCipher += decMin;
+    }
+
+    else {
+      let others = String.fromCharCode(codAsc);
+      decoderCipher += others;
+    }
+  }
+  return decoderCipher;
+}
+
+function printDecode() {
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("FinalMessage2").style.display = "block";
+
+  message = document.getElementById("message").value;
+  offset = parseInt(document.getElementById("offset").value);
+ 
+  document.getElementById("resultDecoder").innerHTML = cipherDecode(message, offset);
+}
+
+function goBack() {
+  document.getElementById("homepage").style.display = "block";
+  document.getElementById("FinalMessage").style.display = "none";
+  document.getElementById("FinalMessage2").style.display = "none";
+}
+
+
---
+++ src/tests.js
@@ -0,0 +1,49 @@
+//Teste da função codificar
+/*var resultado = cipherEncode("abcdefghijklmnopqrstuvwxyz", -1)
+if (resultado === "zabcdefghijklmnopqrstuvwxy"){
+    console.log("Ok!!!")
+} else{
+    //("Não passou! Resposta esperada: " + "zabcdefghijklmnopqrstuvwx")
+    console.log(Error)
+}
+
+
+var resultado = cipherEncode("abc", 10)
+if (resultado === "klm"){
+    console.log("Deu certo!" + resultado)
+} else{
+    console.log(Error)
+}
+
+var resultado = cipherEncode("!! Oi", 3)
+if (resultado === "!! Rl" ){
+    console.log("Deu certo!" + resultado)
+} else{
+    console.log(Error)
+}
+
+var resultado = cipherEncode("ÉéÁáàÀ 1234 MEI!!", 7)
+if (resultado === "ÉéÁáàÀ 1234 TLP!!" ){
+    console.log("Deu certo!" + resultado)
+} else{
+    console.log(Error)
+}
+
+//Teste da função decodificar
+var resultado = cipherDecode("ÉéÁáàÀ 1234 MEI!!", 1)
+if (resultado === "ÉéÁáàÀ 1234 LDH!!" ){
+    console.log("Deu certo!" + resultado)
+} else{
+    console.log(Error)
+}
+
+var resultado = cipherDecode("Carolina!123", -3)
+if (resultado === "ÉéÁáàÀ 1234 LDH!!" ){
+    console.log("Deu certo!" + resultado)
+} else{
+    console.log(Error)
+}
+
+var resultado = cipherDecode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", )*/
+
+//testar símbolos, bordas e meios , testar os outros botões como voltar
