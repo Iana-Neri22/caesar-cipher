7ef951642dfd26c79e1e636d337a3280bf13ee09
Iana-Neri22 <iananeri@outlook.com>
2019-06-21 14:52:34 -0300

    let replaced with const

--- src/cipher.js
+++ src/cipher.js
@@ -16,13 +16,13 @@ function printDecode(){
 function cipherEncode(message, offset){
   let codeCipher = '';
   for (let i in message) {
-    let codAsc = message.charCodeAt(i);
+    const codAsc = message.charCodeAt(i);
     if (codAsc >= 65 && codAsc <= 90) {
-      let encodeMai = (codAsc - 65 + (offset % 26) + 26) % 26 + 65;
+      const encodeMai = (codAsc - 65 + (offset % 26) + 26) % 26 + 65;
       codeCipher += String.fromCharCode(encodeMai);
     }
     else if (codAsc >= 97 && codAsc <= 122) {
-      let encodeMin = (codAsc - 97 + (offset % 26) + 26) % 26 + 97;
+      const encodeMin = (codAsc - 97 + (offset % 26) + 26) % 26 + 97;
       codeCipher += String.fromCharCode(encodeMin);
     }
     else {
@@ -35,13 +35,13 @@ function cipherEncode(message, offset){
 function cipherDecode(message, offset) {
   let decoderCipher = '';
   for (let i in message) {
-    let codAsc = message.charCodeAt(i);
+    const codAsc = message.charCodeAt(i);
     if (codAsc >= 65 && codAsc <= 90) {
-      let decoderMai = (codAsc - 65 - (offset % 26) + 26) % 26 + 65;
+      const decoderMai = (codAsc - 65 - (offset % 26) + 26) % 26 + 65;
       decoderCipher += String.fromCharCode(decoderMai);
     }
     else if (codAsc >= 97 && codAsc <= 122) {
-      let decoderMin = (codAsc - 97 - (offset % 26) + 26) % 26 + 97;
+      const decoderMin = (codAsc - 97 - (offset % 26) + 26) % 26 + 97;
       decoderCipher += String.fromCharCode(decoderMin);
     }
     else {
